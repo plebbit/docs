eebf4277953a5a834d44c004d92786dd5d80091c
Rinse <rinse12@tutanota.com>
2024-05-17 09:38:59 +0000

    add edit url to doc

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,17 +37,9 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: (arg) => {
+            return `https://github.com/plebbit/plebbit-docs/blob/master/docs/${arg.docPath}`;
+          },
         },
         theme: {
           customCss: "./src/css/custom.css",
