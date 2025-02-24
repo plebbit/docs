554c48cc72bb14362c33ec9572f5ca5cad035c32
Tom (plebeius.eth) <tom@plebbit.com>
2025-02-24 22:07:57 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "Plebbit Docs",
-  tagline: "Learn how Plebbit decentralizes social media and how to build on it",
+  tagline: "Learn how Plebbit's protocol powers serverless, adminless, fully decentralized social media, and how to build your own",
   favicon: "img/logo.png",
 
   // Set the production url of your site here
@@ -52,6 +52,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
     navbar: {
       title: "Plebbit Docs",
       logo: {
